851ef86a1420a04aa91b9119b24d13217638853d
Tipwheal <674714966@qq.com>
2016-11-24 11:05:40 +0800

    11.24我就水一水
    加了个登录结果的enum到VO里
    改好登录接口
    并写了一点点

--- HMSBl/src/Login_bl_servlmpl/LoginBlServlmpl.java
+++ HMSBl/src/Login_bl_servlmpl/LoginBlServlmpl.java
@@ -2,25 +2,54 @@ package Login_bl_servlmpl;
 
 import Login_bl_serv.LoginBlServ;
 import PO.UserPO;
+import VO.LoginResult;
+import VO.UserVO;
+import rmi.RemoteHelper;
+
+import java.rmi.RemoteException;
 
 public class LoginBlServlmpl implements LoginBlServ{
 
 	@Override
-	public boolean isCorrect(String id, String password) {
-		// TODO Auto-generated method stub
+	/**
+	 * 登录方法，如果没有这个用户，返回wrongId
+	 * 如果密码不正确，返回wrongPassword
+	 * 登录成功，返回success
+	 */
+	public LoginResult login(String id, String password) {
+		UserPO userPO = null;
+		try {
+			userPO = RemoteHelper.getInstance().getUserDateServ().getUser(id);
+		}catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		if(userPO == null) {
+			return LoginResult.wrongId;
+		}
+		boolean correct = password.equals(userPO.getPassword());
+		if(correct) {
+			return LoginResult.success;
+		}else {
+			return LoginResult.wrongPassword;
+		}
+	}
+
+	@Override
+	/**
+	 * 注册一个用户，成功返回true
+	 * 失败返回false
+	 */
+	public boolean register(UserVO user) {
 		return false;
 	}
 
 	@Override
-	public boolean register(UserPO user) {
-		// TODO Auto-generated method stub
+	public boolean logout(String id) {
 		return false;
 	}
 
 	@Override
-	public UserPO getUserInfo(String id) {
-		// TODO Auto-generated method stub
+	public UserVO getUserInfo(String id) {
 		return null;
 	}
-
 }
---
+++ HMSBl/src/VO/LoginResult.java
@@ -0,0 +1,8 @@
+package VO;
+
+/**
+ * Created by Administrator on 2016/11/24.
+ */
+public enum LoginResult {
+    wrongPassword,success,wrongId
+}
